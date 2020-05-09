7898ec37e0398e63b2c4fd8e6983c930d5ac88b4
Carter Cook <13cjcc@gmail.com>
2020-05-09 13:39:06 -0400

    Initial Commit

---
+++ Assets/AnimateDialogue.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+[RequireComponent(typeof(AudioSource))]
+public class AnimateDialogue : MonoBehaviour
+{
+    public string[] lines;
+
+    public AudioClip[] soundClips;
+
+    private int lineNum = 0;
+
+    private int charNum = 0;
+
+    private Text text;
+
+    private AudioSource audio;
+
+    private Coroutine coroutine;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        audio = GetComponent<AudioSource>();
+
+        Run();
+    }
+
+    public void Run()
+    {
+        coroutine = StartCoroutine(printChar());
+    }
+
+    public void Stop()
+    {
+        StopCoroutine(coroutine);
+    }
+
+    /// A coroutine that animates text in a textbox
+    private IEnumerator printChar()
+    {
+        yield return setTextAndWait();
+
+        while (true)
+        {
+            charNum++;
+
+            if (charNum >= lines[lineNum].Length)
+            {
+                lineNum++;
+                charNum = 0;
+
+                if (lineNum >= lines.Length)
+                {
+                    break;
+                }
+
+                // idle until mousedown
+                while (!Input.GetMouseButtonDown(0))
+                {
+                    yield return null;
+                }
+            }
+
+            yield return setTextAndWait();
+        }
+
+        lineNum = 0;
+    }
+
+    /// Updates the text component and waits for a fraction of a second
+    private WaitForSeconds setTextAndWait() {
+        if (char.IsLetterOrDigit(lines[lineNum][charNum])) {
+            audio.clip = soundClips[Random.Range(0, soundClips.Length - 1)];
+            audio.Play();
+        }
+
+        text.text = lines[lineNum].Substring(0, charNum + 1);
+        return new WaitForSeconds(0.05f);
+    }
+}
---
+++ Assets/Checklist.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class Checklist : MonoBehaviour
+{
+    public string title = "Checklist";
+
+    public string bullet = " • ";
+
+    public string[] tasks;
+
+    private TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        updateText();
+    }
+
+    void OnEnable()
+    {
+        updateText();
+    }
+
+    private void updateText()
+    {
+        string separator = $"\n{bullet}";
+        string joinedTasks = string.Join(separator, tasks);
+        text.text = $"{title}{separator}{joinedTasks}";
+    }
+}
