ea6ad4f044cad8b1921fdfb526a97a658195d17e
Carter Cook <13cjcc@gmail.com>
2020-05-10 13:28:35 -0400

    commit

--- Assets/Scripts/WalkAnimationState.cs
+++ Assets/Scripts/WalkAnimationState.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using DigitalRuby.Tween;
 
 [RequireComponent(typeof(FrameAnimator))]
 public class WalkAnimationState : MonoBehaviour
@@ -20,15 +21,44 @@ public class WalkAnimationState : MonoBehaviour
 
     private RectTransform rectTransform;
 
+    private RectTransform characterTransform;
+
     void Awake()
     {
         frameAnimator = GetComponent<FrameAnimator>();
         rectTransform = GetComponentInParent<Room>().GetComponent<RectTransform>();
+        characterTransform = GetComponentInParent<CharacterRoot>().GetComponent<RectTransform>();
     }
 
     void OnEnable()
     {
         StartCoroutine(playFrames());
+
+        print($"{rectTransform.offsetMin.x}, {rectTransform.sizeDelta.x}");
+
+        TweenFactory.Tween(
+            null,
+            characterTransform.position.x,
+            rectTransform.offsetMin.x + rectTransform.sizeDelta.x * Random.value,
+            1,
+            TweenScaleFunctions.QuadraticEaseInOut,
+            (ITween<float> t) =>
+            {
+                Vector3 pos = characterTransform.position;
+                pos.x = t.CurrentValue;
+                characterTransform.position = pos;
+            },
+            (t) =>
+            {
+                enabled = false;
+                nextState.enabled = true;
+            });
+    }
+
+    private void goToNextState()
+    {
+        enabled = false;
+        nextState.enabled = true;
     }
 
     private IEnumerator playFrames()
@@ -39,7 +69,6 @@ public class WalkAnimationState : MonoBehaviour
 
         while (elapsed < time)
         {
-            print(elapsed + ", " + time);
             yield return null;
             updateFrame();
         }
@@ -56,12 +85,5 @@ public class WalkAnimationState : MonoBehaviour
 
         frameAnimator.frameNumber = frameIndex;
         elapsed += Time.deltaTime;
-    }
-
-    void Update()
-    {
-        Vector3 pos = rectTransform.position;
-        pos.x += (distance / time) * Time.deltaTime;
-        rectTransform.position = pos;
     }
 }
