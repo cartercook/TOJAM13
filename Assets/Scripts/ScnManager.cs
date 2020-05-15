a35d2d59597c9906d31c1e1712237c840727a2dc
Carter Cook <13cjcc@gmail.com>
2020-05-15 18:54:47 -0400

    Rui changes

---
+++ Assets/Scripts/ScnManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScnManager : MonoBehaviour
+{
+ public void LoadScene(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
