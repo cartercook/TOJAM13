7d9e9465a531fe57ead1fcb0f2ef17cc6df485e0
Carter Cook <13cjcc@gmail.com>
2020-05-19 00:40:04 -0400

    Kite

---
+++ Assets/Scripts/KiteTween.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using DigitalRuby.Tween;
+
+public class KiteTween : MonoBehaviour
+{
+    Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        doTween();
+    }
+
+    private void doTween()
+    {
+        TweenFactory.Tween(
+            null,
+            transform.position,
+            startPos + Random.insideUnitCircle * 4,
+            Random.Range(3, 5),
+            TweenScaleFunctions.SineEaseInOut,
+            (ITween<Vector2> t) => transform.position = t.CurrentValue,
+            (t) => doTween());
+    }
+}
